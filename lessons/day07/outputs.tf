d989d27e54facc68800be222ffe97c53f9a63ddd
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-02 07:21:56 -1000

    Refactor VPC and EC2 outputs for clarity

--- lessons/day07/outputs.tf
+++ lessons/day07/outputs.tf
@@ -1,55 +1,66 @@
-# VPC outputs
+# =========================
+# VPC / Network outputs
+# =========================
+
 output "vpc_id" {
   description = "ID of the VPC"
   value       = aws_vpc.main.id
 }
 
 output "vpc_cidr" {
-  description = "CIDR block of the VPC"
+  description = "CIDR of the VPC"
   value       = aws_vpc.main.cidr_block
 }
 
-# Subnet outputs
 output "subnet_id" {
-  description = "ID of the subnet"
-  value       = aws_subnet.internal.id
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
 }
 
 output "subnet_cidr" {
-  description = "CIDR block of the subnet"
-  value       = aws_subnet.internal.cidr_block
+  description = "CIDR of the public subnet"
+  value       = aws_subnet.public.cidr_block
 }
 
+# =========================
 # Security Group outputs
+# =========================
+
 output "security_group_id" {
-  description = "ID of the security group"
-  value       = aws_security_group.main.id
+  description = "ID of the web security group"
+  value       = aws_security_group.web_sg.id
 }
 
+# =========================
 # EC2 Instance outputs
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.main.id
+# =========================
+
+output "web_server_instance_ids" {
+  description = "IDs of the web server instances"
+  value       = [for i in aws_instance.web_server : i.id]
 }
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.main.public_ip
+output "web_server_public_ips" {
+  description = "Public IPs of the web server instances"
+  value       = [for i in aws_instance.web_server : i.public_ip]
 }
 
-output "instance_private_ip" {
-  description = "Private IP address of the EC2 instance"
-  value       = aws_instance.main.private_ip
+output "web_server_private_ips" {
+  description = "Private IPs of the web server instances"
+  value       = [for i in aws_instance.web_server : i.private_ip]
 }
 
-output "instance_type" {
-  description = "Instance type of the EC2 instance"
-  value       = aws_instance.main.instance_type
+output "web_server_instance_type" {
+  description = "Instance type of the web server"
+  value       = aws_instance.web_server[0].instance_type
 }
 
-# Outputs demonstrating type usage
+# =========================
+# Type examples (by variable)
+# =========================
+
 output "environment_info" {
-  description = "Environment information from string type variable"
+  description = "Example of string type"
   value = {
     name         = var.environment
     type         = "string"
@@ -59,58 +70,35 @@ output "environment_info" {
 }
 
 output "storage_info" {
-  description = "Storage information from number type variable"
+  description = "Example of number type"
   value = {
-    disk_size_gb = var.storage_disk
-    disk_size_mb = var.storage_disk * 1024
+    disk_size_gb = var.storage_size
+    disk_size_mb = var.storage_size * 1024
     type         = "number"
   }
 }
 
-output "deletion_policy" {
-  description = "Deletion policy from boolean type variable"
+output "monitoring_settings" {
+  description = "Example of bool type"
   value = {
-    delete_on_termination = var.is_delete
-    policy_text           = var.is_delete ? "Volumes will be deleted" : "Volumes will be retained"
-    type                  = "bool"
+    enable_monitoring   = var.enable_monitoring
+    associate_public_ip = var.associate_public_ip
+    type                = "bool"
   }
 }
 
-output "allowed_regions" {
-  description = "Allowed regions from list type variable"
+output "allowed_cidr_info" {
+  description = "Example of list(string) type"
   value = {
-    regions      = var.allowed_locations
-    region_count = length(var.allowed_locations)
-    primary      = var.allowed_locations[0]
-    type         = "list(string)"
-  }
-}
-
-output "tags_info" {
-  description = "Tags from map type variable"
-  value = {
-    tags       = var.resource_tags
-    tag_count  = length(keys(var.resource_tags))
-    tag_keys   = keys(var.resource_tags)
-    tag_values = values(var.resource_tags)
-    type       = "map(string)"
-  }
-}
-
-output "network_configuration" {
-  description = "Network configuration from tuple type variable"
-  value = {
-    tuple_value   = var.network_config
-    vpc_cidr      = element(var.network_config, 0)
-    subnet_prefix = element(var.network_config, 1)
-    cidr_bits     = element(var.network_config, 2)
-    subnet_full   = "${element(var.network_config, 1)}/${element(var.network_config, 2)}"
-    type          = "tuple([string, string, number])"
+    cidr_blocks = var.allowed_cidr_blocks
+    first_cidr  = var.allowed_cidr_blocks[0]
+    cidr_count  = length(var.allowed_cidr_blocks)
+    type        = "list(string)"
   }
 }
 
 output "instance_types_info" {
-  description = "Instance types from list type variable"
+  description = "Example of list(string) for instance types"
   value = {
     allowed_types = var.allowed_instance_types
     count         = length(var.allowed_instance_types)
@@ -119,19 +107,57 @@ output "instance_types_info" {
   }
 }
 
-output "vm_configuration" {
-  description = "VM configuration from object type variable"
+output "tags_info" {
+  description = "Example of map(string) type"
   value = {
-    config        = var.vm_config
-    instance_type = var.vm_config.instance_type
-    ami_id        = var.vm_config.ami_id
-    monitoring    = var.vm_config.monitoring
-    type          = "object"
+    tags       = var.instance_tags
+    tag_count  = length(keys(var.instance_tags))
+    tag_keys   = keys(var.instance_tags)
+    tag_values = values(var.instance_tags)
+    type       = "map(string)"
   }
-  sensitive = false
 }
 
+output "availability_zones_info" {
+  description = "Example of set(string) type"
+  value = {
+    zones      = var.availability_zones
+    zone_count = length(var.availability_zones)
+    zones_list = tolist(var.availability_zones)
+    type       = "set(string)"
+  }
+}
+
+output "network_configuration" {
+  description = "Example of tuple([string, string, number]) type"
+  value = {
+    tuple_value   = var.network_config
+    vpc_cidr      = var.network_config[0]
+    subnet_prefix = var.network_config[1]
+    cidr_bits     = var.network_config[2]
+    subnet_full   = "${var.network_config[1]}/${var.network_config[2]}"
+    type          = "tuple([string, string, number])"
+  }
+}
+
+output "server_configuration" {
+  description = "Example of object type"
+  value = {
+    config        = var.server_config
+    name          = var.server_config.name
+    instance_type = var.server_config.instance_type
+    monitoring    = var.server_config.monitoring
+    storage_gb    = var.server_config.storage_gb
+    backup        = var.server_config.backup_enabled
+    type          = "object"
+  }
+}
+
+# =========================
+# Common Tags Output
+# =========================
+
 output "all_resource_tags" {
-  description = "All tags applied to resources (merged common_tags + resource_tags)"
+  description = "Common tags applied to resources"
   value       = local.common_tags
 }
