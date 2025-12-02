d24fb24af9bac477af5fcf3e6c7dfb1c548bd102
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-02 07:09:14 -1000

    Refactor common tags in local.tf
    Updated common tags in local.tf to include project and owner.

--- lessons/day08/code/local.tf
+++ lessons/day08/code/local.tf
@@ -1,11 +1,11 @@
+# local.tf
+# Common tags reused across resources.
+
 locals {
   common_tags = {
-    environment = var.environment
-    lob         = "banking"
-    stage       = "alpha"
-    managed_by  = "terraform"
+    Project     = "terraform-meta-args"
+    Environment = var.environment
+    Owner       = var.owner
+    ManagedBy   = "Terraform"
   }
-
-  # Combine common tags with resource-specific tags
-  merged_tags = merge(local.common_tags, var.resource_tags)
 }
