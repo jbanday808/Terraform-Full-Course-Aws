3b48679caf2dc072b9c728b2937ddcb52059a65e
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-02 07:24:50 -1000

    Update provider.tf

--- lessons/day07/provider.tf
+++ lessons/day07/provider.tf
@@ -8,15 +8,5 @@ terraform {
 }
 
 provider "aws" {
-  region = var.allowed_locations[0]
-
-  default_tags {
-    tags = {
-      Project     = "Terraform-Full-Course-AWS"
-      Day         = "07"
-      Topic       = "Type-Constraints"
-      ManagedBy   = "Terraform"
-      Environment = var.environment
-    }
-  }
+  region = "us-east-1"
 }
