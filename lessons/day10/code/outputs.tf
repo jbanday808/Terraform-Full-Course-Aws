10a936b0b87a9b4a015b7f8465127ccd9757050b
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-04 07:30:21 -1000

    Update outputs.tf

--- lessons/day10/code/outputs.tf
+++ lessons/day10/code/outputs.tf
@@ -1,36 +1,46 @@
 ############################################
-# Outputs – All Demos Active
+# OUTPUTS – DAY 10 TERRAFORM EXPRESSIONS DEMO
+# Includes: Conditional, Dynamic, and Splat
 ############################################
 
-# Demo 1 – Conditional Expression
+############################################
+# DEMO 1 – CONDITIONAL EXPRESSION OUTPUTS
+############################################
+
 output "conditional_instance_type" {
-  description = "Instance type selected using conditional expression."
+  description = "Instance type selected using the conditional expression (dev=t2.micro, prod=t3.large)."
   value       = aws_instance.conditional_example.instance_type
 }
 
 output "conditional_instance_id" {
-  description = "ID of the conditional demo instance."
+  description = "EC2 instance ID created by the conditional expression demo."
   value       = aws_instance.conditional_example.id
 }
 
-# Demo 2 – Dynamic Block
+############################################
+# DEMO 2 – DYNAMIC BLOCK OUTPUTS
+############################################
+
 output "dynamic_security_group_id" {
-  description = "Security group ID created with dynamic ingress rules."
+  description = "Security group ID created using dynamic ingress rules."
   value       = aws_security_group.dynamic_sg.id
 }
 
 output "dynamic_security_group_rules_count" {
-  description = "Number of ingress rules created dynamically."
+  description = "Total number of dynamic ingress rules added to the security group."
   value       = length(var.ingress_rules)
 }
 
-# Demo 3 – Splat Expressions
+############################################
+# DEMO 3 – SPLAT EXPRESSION OUTPUTS
+############################################
+
 output "splat_instance_ids" {
-  description = "List of instance IDs created by splat example."
+  description = "List of instance IDs created from the splat example."
   value       = local.all_instance_ids
 }
 
 output "splat_private_ips" {
-  description = "List of private IPs created by splat example."
+  description = "List of private IP addresses for all splat-created instances."
   value       = local.all_private_ips
 }
