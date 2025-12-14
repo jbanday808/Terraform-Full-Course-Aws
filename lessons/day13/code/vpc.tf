ff157b677c96c73a9a3282000f5df78059102e85
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-14 13:34:26 -1000

    Update vpc.tf

--- lessons/day13/code/vpc.tf
+++ lessons/day13/code/vpc.tf
@@ -1,46 +1,62 @@
-resource "aws_vpc" "eks_vpc" {
-  cidr_block           = "10.0.0.0/16"
+resource "aws_vpc" "shared_network_vpc" {
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "day13-eks-vpc"
+    Name = var.shared_vpc_name
   }
 }
 
-resource "aws_subnet" "eks_primary_subnet" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+resource "aws_internet_gateway" "shared_igw" {
+  vpc_id = aws_vpc.shared_network_vpc.id
 
   tags = {
-    Name = "day13-eks-primary-subnet"
+    Name = "${var.shared_vpc_name}-igw"
   }
 }
 
-resource "aws_internet_gateway" "eks_igw" {
-  vpc_id = aws_vpc.eks_vpc.id
-
-  tags = {
-    Name = "day13-eks-igw"
-  }
-}
-
-resource "aws_route_table" "eks_public_rt" {
-  vpc_id = aws_vpc.eks_vpc.id
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.shared_network_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.eks_igw.id
+    gateway_id = aws_internet_gateway.shared_igw.id
   }
 
   tags = {
-    Name = "day13-eks-public-rt"
+    Name = "${var.shared_vpc_name}-public-rt"
   }
 }
 
-resource "aws_route_table_association" "eks_public_rt_assoc" {
-  subnet_id      = aws_subnet.eks_primary_subnet.id
-  route_table_id = aws_route_table.eks_public_rt.id
+resource "aws_subnet" "subnet_1" {
+  vpc_id                  = aws_vpc.shared_network_vpc.id
+  cidr_block              = var.subnet_1_cidr
+  availability_zone       = var.az_1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet_1_name
+  }
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id                  = aws_vpc.shared_network_vpc.id
+  cidr_block              = var.subnet_2_cidr
+  availability_zone       = var.az_2
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet_2_name
+  }
+}
+
+resource "aws_route_table_association" "subnet_1_assoc" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "subnet_2_assoc" {
+  subnet_id      = aws_subnet.subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
 }
