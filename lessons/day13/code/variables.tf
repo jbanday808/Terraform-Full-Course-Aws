a0790db2cf7ebd7b757e0c18a3274eac9d0b25b4
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-14 13:23:35 -1000

    Update variables.tf

--- lessons/day13/code/variables.tf
+++ lessons/day13/code/variables.tf
@@ -1,5 +1,17 @@
 variable "region" {
-  description = "AWS region"
+  description = "AWS region where the shared EKS VPC and resources exist"
   type        = string
   default     = "us-east-1"
+}
+
+variable "eks_vpc_name" {
+  description = "Tag name of the existing shared EKS VPC"
+  type        = string
+  default     = "shared-eks-vpc"
+}
+
+variable "eks_subnet_name" {
+  description = "Tag name of the existing shared EKS subnet"
+  type        = string
+  default     = "shared-eks-primary-subnet"
 }
