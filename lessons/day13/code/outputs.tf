8a91c1d597fe7d64e119882af9bd142a1211b4e2
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-14 13:35:31 -1000

    Update outputs.tf

--- lessons/day13/code/outputs.tf
+++ lessons/day13/code/outputs.tf
@@ -1,9 +1,24 @@
-output "instance_id" {
-  description = "ID of the EC2 instance launched into the shared EKS subnet"
-  value       = aws_instance.day13_ec2.id
+output "shared_network_vpc_id" {
+  description = "ID of the shared network VPC"
+  value       = aws_vpc.shared_network_vpc.id
 }
 
-output "instance_private_ip" {
-  description = "Private IP address of the EC2 instance"
-  value       = aws_instance.day13_ec2.private_ip
+output "subnet_1_id" {
+  description = "ID of subnet-1"
+  value       = aws_subnet.subnet_1.id
+}
+
+output "subnet_2_id" {
+  description = "ID of subnet-2"
+  value       = aws_subnet.subnet_2.id
+}
+
+output "instance_ids" {
+  description = "EC2 instance IDs"
+  value       = [for i in aws_instance.shared_network_ec2 : i.id]
+}
+
+output "instance_private_ips" {
+  description = "EC2 private IPs"
+  value       = [for i in aws_instance.shared_network_ec2 : i.private_ip]
 }
