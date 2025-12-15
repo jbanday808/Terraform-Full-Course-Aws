714cc416f089f287b12a1a699e5a301dcc190333
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-14 14:38:05 -1000

    Update variables.tf

--- lessons/day14/variables.tf
+++ lessons/day14/variables.tf
@@ -1,12 +1,33 @@
-variable "aws_region" {
-  description = "The AWS region to create resources in."
+variable "region" {
+  description = "AWS region where resources will be deployed"
   type        = string
   default     = "us-east-1"
 }
 
-variable "bucket_prefix" {
-  description = "Prefix for the S3 bucket name."
+variable "bucket_name" {
+  description = "S3 bucket name used to host the static website"
   type        = string
-  default     = "my-static-website-"
+  default     = "day14-static-website"
 }
 
+variable "environment" {
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "prod"
+}
+
+variable "project_name" {
+  description = "Project identifier used for tagging and naming"
+  type        = string
+  default     = "day14-static-website"
+}
+
+variable "tags" {
+  description = "Common tags applied to all AWS resources"
+  type        = map(string)
+  default = {
+    Project     = "day14-static-website"
+    Environment = "prod"
+    ManagedBy   = "Terraform"
+  }
+}
