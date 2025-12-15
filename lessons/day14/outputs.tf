ae15c550af8bbd941720d47374895ed970ee2457
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-14 14:41:16 -1000

    Update outputs.tf

--- lessons/day14/outputs.tf
+++ lessons/day14/outputs.tf
@@ -1,14 +1,24 @@
 output "website_url" {
-  description = "The URL of the static website"
+  description = "The CloudFront URL for the static website"
   value       = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
 }
 
+output "cloudfront_domain_name" {
+  description = "The CloudFront domain name"
+  value       = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
 output "cloudfront_distribution_id" {
-  description = "The ID of the CloudFront distribution"
+  description = "The CloudFront distribution ID"
   value       = aws_cloudfront_distribution.s3_distribution.id
 }
 
 output "s3_bucket_name" {
-  description = "The name of the S3 bucket"
+  description = "The S3 bucket name hosting the website content"
   value       = aws_s3_bucket.website.bucket
+}
+
+output "s3_bucket_arn" {
+  description = "The S3 bucket ARN"
+  value       = aws_s3_bucket.website.arn
 }
