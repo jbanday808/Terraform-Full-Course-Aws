244f84c315c20620e9b0cf52a8558fa8319185b1
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-18 13:47:08 -1000

    Update versions.tf

--- lessons/day16/versions.tf
+++ lessons/day16/versions.tf
@@ -1,8 +1,19 @@
+#############################################
+# Provider Versions (versions.tf)
+# Purpose: Lock provider sources and versions
+#          for consistent Terraform behavior.
+#############################################
+
+#############################
+# 1) Required Providers
+#############################
+
 terraform {
+  # LABEL: Required provider definitions
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source  = "hashicorp/aws" # LABEL: AWS provider source
+      version = "~> 5.0"        # LABEL: AWS provider version constraint
     }
   }
 }
