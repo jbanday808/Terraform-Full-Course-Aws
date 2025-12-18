2cef9546079d1ead7848aa2257c596b903630b9f
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-18 13:38:26 -1000

    Update groups.tf

--- lessons/day16/groups.tf
+++ lessons/day16/groups.tf
@@ -1,45 +1,72 @@
-# Create IAM Groups
+#############################################
+# IAM Groups and Memberships (groups.tf)
+# Purpose: Create IAM groups and automatically
+#          assign users based on tags from users.csv
+#############################################
+
+#############################
+# 1) IAM Groups
+#############################
+
+# Label: Education IAM group
 resource "aws_iam_group" "education" {
   name = "Education"
   path = "/groups/"
 }
 
+# Label: Managers IAM group
 resource "aws_iam_group" "managers" {
   name = "Managers"
   path = "/groups/"
 }
 
+# Label: Engineers IAM group
 resource "aws_iam_group" "engineers" {
   name = "Engineers"
   path = "/groups/"
 }
 
-# Add users to the Education group
+#############################
+# 2) Group Memberships
+#############################
+
+# Label: Automatically add users to Education group
+# Criteria: Department tag equals "Education"
 resource "aws_iam_group_membership" "education_members" {
   name  = "education-group-membership"
   group = aws_iam_group.education.name
 
   users = [
-    for user in aws_iam_user.users : user.name if user.tags.Department == "Education"
+    for user in aws_iam_user.users :
+    user.name
+    if user.tags.Department == "Education"
   ]
 }
 
-# Add users to the Managers group
+# Label: Automatically add users to Managers group
+# Criteria: JobTitle contains "Manager" or "CEO"
 resource "aws_iam_group_membership" "managers_members" {
   name  = "managers-group-membership"
   group = aws_iam_group.managers.name
 
   users = [
-    for user in aws_iam_user.users : user.name if contains(keys(user.tags), "JobTitle") && can(regex("Manager|CEO", user.tags.JobTitle))
+    for user in aws_iam_user.users :
+    user.name
+    if contains(keys(user.tags), "JobTitle")
+    && can(regex("Manager|CEO", user.tags.JobTitle))
   ]
 }
 
-# Add users to the Engineers group
+# Label: Automatically add users to Engineers group
+# Criteria: Department tag equals "Engineering"
+# Note: No users currently match this condition in users.csv
 resource "aws_iam_group_membership" "engineers_members" {
   name  = "engineers-group-membership"
   group = aws_iam_group.engineers.name
 
   users = [
-    for user in aws_iam_user.users : user.name if user.tags.Department == "Engineering" # Note: No users match this in the current CSV
+    for user in aws_iam_user.users :
+    user.name
+    if user.tags.Department == "Engineering"
   ]
 }
