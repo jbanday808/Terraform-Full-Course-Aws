e0982b9b98fc7ad93286bde09a4452ba64cfc1f2
James Banday <45937484+jbanday808@users.noreply.github.com>
2025-12-20 10:42:51 -1000

    Update variable descriptions for clarity

--- lessons/day17/variables.tf
+++ lessons/day17/variables.tf
@@ -1,8 +1,16 @@
+#############################################
+# AWS Region Configuration
+#############################################
+
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region where all resources will be deployed"
   type        = string
   default     = "us-east-1"
 }
+
+#############################################
+# Application Configuration
+#############################################
 
 variable "app_name" {
   description = "Name of the Elastic Beanstalk application"
@@ -10,22 +18,36 @@ variable "app_name" {
   default     = "my-app-bluegreen"
 }
 
+#############################################
+# Elastic Beanstalk Platform Configuration
+#############################################
+
 variable "solution_stack_name" {
-  description = "Elastic Beanstalk solution stack name (platform)"
+  description = "Elastic Beanstalk solution stack (platform and runtime)"
   type        = string
-  # Node.js 20 running on 64bit Amazon Linux 2023
+
+  # Node.js 20 running on 64-bit Amazon Linux 2023
   default = "64bit Amazon Linux 2023 v6.6.8 running Node.js 20"
 }
 
+#############################################
+# EC2 Instance Configuration
+#############################################
+
 variable "instance_type" {
-  description = "EC2 instance type for Elastic Beanstalk environments"
+  description = "EC2 instance type used by Elastic Beanstalk environments"
   type        = string
   default     = "t3.micro"
 }
 
+#############################################
+# Resource Tagging Configuration
+#############################################
+
 variable "tags" {
-  description = "Tags to apply to all resources"
+  description = "Common tags applied to all Terraform-managed resources"
   type        = map(string)
+
   default = {
     Project     = "BlueGreenDeployment"
     Environment = "Demo"
